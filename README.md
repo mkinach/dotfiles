62ddc7be23c623be025cb29c90e488b82a0c3521
mpkin <mpkin@users.noreply.github.com>
2023-08-26 14:25:17 -0700

    Updates

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # dotfiles
 
-My basic set of dotfiles (and other configs) to get up-and-running on a new machine.
+My personal dotfiles (and other configs) to get up-and-running on a new machine.
 
 To use, clone this repo and then run `./install.sh` followed by `./setup.sh`.
 
--- install.sh
+++ install.sh
@@ -2,17 +2,30 @@
 
 # install.sh: automatically installs necessary software
 
+# get distro name (stored in $ID)
+. /etc/os-release
+
 read -r -p "Update the entire system? (Y/n): " ANS_UPDATE
 case ${ANS_UPDATE} in
-  Y) sudo apt -y update
-     sudo apt -y upgrade
-     ;;
+  Y) case ${ID} in
+       ubuntu) sudo apt -y update
+               sudo apt -y upgrade ;;
+       mageia) su -c "urpmi --force --auto-update" ;;
+     esac ;;
   *) ;;
 esac
 
-while read -r package; do
-    sudo apt install -y "$package"
-done < pkgs_ubuntu.txt
+case ${ID} in
+   ubuntu)  
+     while read -r package; do
+         sudo apt install -y "$package"
+     done < pkgs_ubuntu.txt
+     ;;
+   mageia) 
+     PACKAGES=$(cat pkgs_mageia.txt | tr '\n' ' ')
+     su -c "urpmi --auto ${PACKAGES}"
+     ;;
+esac
 
 # install starship
 wget -P /tmp https://starship.rs/install.sh && mkdir -p ${HOME}/opt/starship && sh /tmp/install.sh -y -b ${HOME}/opt/starship
